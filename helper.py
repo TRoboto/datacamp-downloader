be9836546fb1466fca479b8cccc18156d9958766
Muhammad Al-Barham <42673854+MuhammadAlBarham@users.noreply.github.com>
2020-03-31 23:53:11 +0300

    beta2

--- helper.py
+++ helper.py
@@ -1,7 +1,8 @@
 import os
 import sys
 import time
-import psutil
+import string
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -15,18 +16,16 @@ class bcolors:
     BKBLUE = '\x1b[7;37;44m'
     BKGREEN = '\x1b[7;37;42m'
     BKENDC = '\x1b[0m'
-    
+
+
 def download_file(con, video_link, location):
     # start = time.clock()
     while True:
-        try :
+        try:
             response = con.session.get(video_link, stream=True)
-        except Exception:
-            print(bcolors.FAIL + "Error occured, trying again...")
-            con.set_new_session()
-            time.sleep(5)
-            continue
-        break
+            break
+        except:
+            handle_error(con)
     mkdir(location)
     if file_exist(location):
         return
@@ -37,20 +36,28 @@ def download_file(con, video_link, location):
         else:
             dl = 0
             total_length = int(total_length)
-            for data in response.iter_content(chunk_size=1024): #1MB
+            for data in response.iter_content(chunk_size=1024 * 1024):  # 1MB
                 dl += len(data)
                 f.write(data)
                 done = int(50 * dl / total_length)
                 sys.stdout.write("'{}{}{}' {}[%s%s] %d%% {}\r"
                                  .format(bcolors.OKBLUE, os.path.basename(location),
-                                         bcolors.ENDC, bcolors.WARNING, bcolors.ENDC) % ('=' * done, 
-                                         ' ' * (50-done), done*2))
-                                        #  dl//(time.clock() - start) / 800000))
+                                         bcolors.ENDC, bcolors.WARNING, bcolors.ENDC) % ('=' * done,
+                                                                                         ' ' * (50 - done), done * 2))
+                #  dl//(time.clock() - start) / 800000))
                 sys.stdout.flush()
     sys.stdout.write('\n')
 
+
 def file_exist(file):
     return os.path.isfile(file)
+
+
+def format_filename(name):
+    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    filename = ''.join(c for c in name if c in valid_chars)
+    return filename
+
 
 def mkdir(location):
     if not os.path.isdir(os.path.dirname(location)):
@@ -59,9 +66,16 @@ def mkdir(location):
         else:
             os.mkdir(location)
 
+
 def embbed_link(link):
     if '?' in link:
         link += '&embedded=true'
     else:
         link += '?embedded=true'
     return link
+
+
+def handle_error(con):
+    print(bcolors.FAIL + "Error occured, trying again...")
+    con.set_new_session()
+    time.sleep(5)
