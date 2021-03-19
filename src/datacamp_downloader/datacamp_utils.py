450dd582eca7e4184b0a38b5669b3ebea000d77b
TRoboto <m4bh@hotmail.com>
2021-03-19 23:38:58 +0200

    fix bug

--- src/datacamp_downloader/datacamp_utils.py
+++ src/datacamp_downloader/datacamp_utils.py
@@ -291,15 +291,16 @@ class Datacamp:
                     exercise, path / "exercises" / f"ex{exercise_counter}.md"
                 )
                 exercise_counter += 1
-            if videos and exercise.is_video:
+            if exercise.is_video:
                 video = self._get_video(exercise.data.get("projector_key"))
                 video_path = path / "videos" / f"ch{chapter.number}_{video_counter}"
-                download_file(
-                    self.session,
-                    video.video_mp4_link,
-                    video_path.with_suffix(".mp4"),
-                    False,
-                )
+                if videos and video.video_mp4_link:
+                    download_file(
+                        self.session,
+                        video.video_mp4_link,
+                        video_path.with_suffix(".mp4"),
+                        False,
+                    )
                 if audios and video.audio_link:
                     download_file(
                         self.session,
