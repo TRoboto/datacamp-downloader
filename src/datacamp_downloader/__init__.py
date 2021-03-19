3f0bdba3c10fc2479176814c4bd4e43688d9e456
TRoboto <m4bh@hotmail.com>
2021-03-20 01:31:26 +0200

    add color support to windows

--- src/datacamp_downloader/__init__.py
+++ src/datacamp_downloader/__init__.py
@@ -1,6 +1,10 @@
 from .session import Session
 import copyreg
 import ssl
+from colorama import init
+
+# use Colorama to make Termcolor work on Windows too
+init()
 
 active_session = Session.load()
 datacamp = active_session.datacamp
