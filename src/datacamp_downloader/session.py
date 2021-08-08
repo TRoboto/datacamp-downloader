2990d1eb61fdc5732d3fb6d50e7ece98ef365e61
Mohammad Al-Fetyani <m4bh@hotmail.com>
2021-08-08 12:27:28 +0300

    Few improvements

--- src/datacamp_downloader/session.py
+++ src/datacamp_downloader/session.py
@@ -1,10 +1,12 @@
-import requests
-import cloudscraper
+import os
 import pickle
 from pathlib import Path
-from .datacamp_utils import Datacamp
+
+import cloudscraper
+import requests
+
 from .constants import SESSION_FILE
-import os
+from .datacamp_utils import Datacamp
 
 
 class Session:
@@ -25,22 +27,21 @@ class Session:
 
     def restart(self):
         headers = {
-            "Accept-Encoding": "gzip, deflate, sdch",
-            "Accept-Language": "en-US,en;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             "Cache-Control": "max-age=0",
             "Connection": "keep-alive",
-            "Referer": "https://learn.datacamp.com/",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0",
-            "Accept-Language": "en-US,en;q=0.5",
+            "Referer": "https://www.google.com/",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+            "(KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
+            "Accept-Language": "en-US,en;q=0.9",
             "Accept-Encoding": "gzip, deflate, br",
             "Sec-Fetch-Dest": "document",
             "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "same-site",
-            "DNT": "1",
-            "Sec-GPC": "1",
-            "TE": "Trailers",
+            "Sec-Fetch-Site": "same-origin",
+            "sec-fetch-user": "?1",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua": '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+            "Upgrade-Insecure-Requests": "1",
         }
         s = requests.Session()
         s.headers = headers
