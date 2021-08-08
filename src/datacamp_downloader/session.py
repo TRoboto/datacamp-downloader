96bff1a6cee5893b5779ad966d9f60424a5624b5
Mohammad Al-Fetyani <m4bh@hotmail.com>
2021-08-08 21:39:08 +0300

    Switch to selenium

--- src/datacamp_downloader/session.py
+++ src/datacamp_downloader/session.py
@@ -1,64 +1,79 @@
+import json
 import os
 import pickle
 from pathlib import Path
 
-import cloudscraper
-import requests
+# import chromedriver_autoinstaller
+import undetected_chromedriver.v2 as uc
+from selenium.webdriver.common.by import By
 
-from .constants import SESSION_FILE
+from .constants import HOME_PAGE, SESSION_FILE
 from .datacamp_utils import Datacamp
+
+# Check if the current version of chromedriver exists
+# and if it doesn't exist, download it automatically,
+# then add chromedriver to path
+# chromedriver_autoinstaller.install()
 
 
 class Session:
     def __init__(self) -> None:
         self.savefile = Path(SESSION_FILE)
-        self.datacamp = Datacamp(self)
+        self.datacamp = self.load_datacamp()
+        self.start()
+        if self.datacamp.token:
+            self.add_token(self.datacamp.token)
 
     def save(self):
-        pickled = pickle.dumps(self)
+        self.datacamp.session = None
+        pickled = pickle.dumps(self.datacamp)
         self.savefile.write_bytes(pickled)
 
-    @staticmethod
-    def load():
-        savefile = Path(SESSION_FILE)
-        if savefile.exists():
-            return pickle.load(savefile.open("rb"))
-        return Session()
+    def load_datacamp(self):
+        if self.savefile.exists():
+            datacamp = pickle.load(self.savefile.open("rb"))
+            datacamp.session = self
+            return datacamp
+        return Datacamp(self)
 
-    def restart(self):
-        headers = {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "Cache-Control": "max-age=0",
-            "Connection": "keep-alive",
-            "Referer": "https://www.google.com/",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-            "(KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "same-origin",
-            "sec-fetch-user": "?1",
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua": '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
-            "Upgrade-Insecure-Requests": "1",
-        }
-        s = requests.Session()
-        s.headers = headers
-        self.session = cloudscraper.create_scraper(s)
-        # remove old session
+    def reset(self):
         try:
-            os.remove(self.savefile)
+            os.remove(SESSION_FILE)
         except:
             pass
-        return self
 
-    def get(self, *args, **kwargs):
-        return self.session.get(*args, **kwargs)
+    def start(self):
+        self.driver = uc.Chrome()
+        self.driver.get(HOME_PAGE)
+
+    def bypass_cloudflare(self, url):
+        with self.driver:
+            self.driver.get(url)
+
+    def get(self, url):
+        self.driver.get(url)
+        try:
+            self.driver.find_element(By.ID, "cf-spinner-allow-5-secs")
+            self.bypass_cloudflare(url)
+        except:
+            pass
+        return self.driver.page_source
+
+    def get_json(self, url):
+        page = self.get(url)
+        page = self.driver.find_element(By.TAG_NAME, "pre").text
+        parsed_json = json.loads(page)
+        return parsed_json
 
     def post(self, *args, **kwargs):
         return self.session.post(*args, **kwargs)
 
-    def add_cookie(self, cookie: dict):
-        self.session.cookies.set(**cookie)
+    def add_token(self, token: str):
+        cookie = {
+            "name": "_dct",
+            "value": token,
+            "domain": ".datacamp.com",
+            "secure": True,
+        }
+        self.driver.add_cookie(cookie)
         return self
